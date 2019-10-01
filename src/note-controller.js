85ac69bd31138f22ca03494492029c1aaa338530
Volker Bergner <volker@bergner.one>
2019-10-01 16:08:57 +0100

    testing node

---
+++ src/note-controller.js
@@ -0,0 +1,10 @@
+// $(document).ready(function() {
+function testme() {console.log("hi")}
+
+console.log("hi");
+// console.log($("#app"));
+// console.log($("#app").html);
+// console.log($("#app").text);
+// $("#app").html = "Howdy";
+
+// })
