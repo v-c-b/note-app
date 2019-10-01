b9765d73490de8c5b34d06ab24ecebab5eb1e10b
Volker Bergner <volker@bergner.one>
2019-10-01 16:27:25 +0100

    simple index

--- src/note-controller.js
+++ src/note-controller.js
@@ -1,10 +1,3 @@
-// $(document).ready(function() {
-function testme() {console.log("hi")}
-
-console.log("hi");
-// console.log($("#app"));
-// console.log($("#app").html);
-// console.log($("#app").text);
-// $("#app").html = "Howdy";
-
-// })
+$(document).ready(function() {
+  $("#app").html("Howdy");
+})
