7bf09de801057f16712ced28b5f0b949b9f78fae
James Holton <james_holton@yahoo.co.uk>
2019-10-02 09:41:00 +0100

    Fixed annoying export problem, and added table append

--- src/note-controller.js
+++ src/note-controller.js
@@ -1,19 +1,17 @@
 $(document).ready(function() {
   $("#app").html("Howdy");
-
-
-  (function(exports){
-    var newNoteList = new noteList();
-    newNoteList.createNote("Favourite drink: seltzer.");
-    
-    function createNoteRow(){
-      this.htmlString = "<tr><td>";
-      // htmlString += newNoteList.returnList().pop().returnText();
-      // htmlString += "</td></tr>";
-      //return "test";
-    }
-    exports.createNoteRow = createNoteRow;
-  })(this);
-
-  console.log(createNoteRow());
 });
+
+(function(exports){
+  var newNoteList = new noteList();
+  newNoteList.createNote("Favourite drink: seltzer.");
+  
+  function createNoteRow(){
+    this.htmlString = "<tr><td>";
+    htmlString += newNoteList.returnList().pop().returnText();
+    htmlString += "</td></tr>";
+    return htmlString;
+  }
+  exports.createNoteRow = createNoteRow;
+})(this);
+
