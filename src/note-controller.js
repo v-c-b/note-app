b91d99a9a418024c602ca658f5887cf9bff7c766
ilimeza <ilianameza530@gmail.com>
2019-10-02 16:35:20 +0100

    load single note page

--- src/note-controller.js
+++ src/note-controller.js
@@ -2,8 +2,9 @@ $(document).ready(function() {
   $("#app").html("Howdy");
 });
 
+var newNoteList = new noteList();
+
 (function(exports){
-  var newNoteList = new noteList();
   newNoteList.createNote("Favourite drink: seltzer.");
   newNoteList.createNote("Favourite drink: Beer");
 
@@ -11,9 +12,30 @@ $(document).ready(function() {
   function createNoteRow(){
     this.htmlString = '<tr><td>';
     htmlString += '<a href=#notes/' + newNoteList.returnList()[newNoteList.returnList().length - 1].returnID() + '>';
-    htmlString += newNoteList.returnList().pop().returnText();
+    htmlString += newNoteList.returnList()[newNoteList.returnList().length - 1].returnText();
     htmlString += '<a/></td></tr>';
     return htmlString;
-  }
+  };
   exports.createNoteRow = createNoteRow;
 })(this);
+
+function listenForHashChange() {
+  window.addEventListener("hashchange", showNoteForCurrentPage)
+};
+listenForHashChange();
+
+function showNoteForCurrentPage() {
+  showNote(getNoteFromUrl(window.location));
+};
+
+function getNoteFromUrl(location) {
+  console.log(location.hash.split("/")[1]);
+  return location.hash.split("/")[1];
+};
+
+function showNote(note) {
+  console.log(newNoteList.returnList()[note].text)
+  document
+    .getElementById("note to display")
+    .innerHTML = newNoteList.returnList()[note].text;
+};
